94bcb2559442b8d4888f9872cd5ab11b8a7c34a3
Anant <anantk17@ymail.com>
2015-01-22 23:22:23 +0530

    cosmetic changes

--- stage3-onwards/exptree.c
+++ stage3-onwards/exptree.c
@@ -2,161 +2,167 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//Create operator nodes
 struct tree_node* mkOpNode(int op, struct tree_node* ptr1, struct tree_node* ptr2)
+{
+    struct tree_node *node = (struct tree_node*) malloc(sizeof(struct tree_node));
+    if(!node)
+        printf("malloc failed");
+    node->type = op;
+    node->value = 0;
+    node->name = 0;
+    node->arglist = NULL;
+    node->ptr1 = ptr1;
+    node->ptr2 = ptr2;
+
+    return node;
+}
+
+//Create statement nodes
+struct tree_node* mkstmtNode(int stmt, struct tree_node* ptr1, struct tree_node* ptr2)
+{
+
+    struct tree_node *node = (struct tree_node*) malloc(sizeof(struct tree_node));
+    if(!node)
+        printf("malloc failed");
+    node->type = stmt;
+    node->value = 0;
+    node->name = 0;
+    if(stmt == CREAD||stmt == CWRITE)
     {
-        struct tree_node *node = (struct tree_node*) malloc(sizeof(struct tree_node));
-        if(!node)
-            printf("malloc failed");
-        node->type = op;
-        node->value = 0;
-        node->name = 0;
+        node->arglist = ptr1;
+        node->ptr1 = NULL;
+        node->ptr2 = NULL;
+    }
+    else
+    {
         node->arglist = NULL;
         node->ptr1 = ptr1;
         node->ptr2 = ptr2;
-
-        return node;
     }
+    
+    return node;
+}
 
-struct tree_node* mkstmtNode(int stmt, struct tree_node* ptr1, struct tree_node* ptr2)
-    {
-
-        struct tree_node *node = (struct tree_node*) malloc(sizeof(struct tree_node));
-        if(!node)
-            printf("malloc failed");
-        node->type = stmt;
-        node->value = 0;
-        node->name = 0;
-        if(stmt == CREAD||stmt == CWRITE)
-        {
-            node->arglist = ptr1;
-            node->ptr1 = NULL;
-            node->ptr2 = NULL;
-        }
-        else
-        {
-            node->arglist = NULL;
-            node->ptr1 = ptr1;
-            node->ptr2 = ptr2;
-        }
-       
-        return node;
-    }
-
+//Create leaf nodes for IDENTIFIERS
 struct tree_node* mkID(char name)
-    {
-        struct tree_node *node = (struct tree_node*)malloc(sizeof(struct tree_node));
-        if(!node)  
-            printf("malloc failed");
-        node->type = 1;
-        node->value = 0;
-        node->name = name;
-        node->arglist = NULL;
-        node->ptr1 = NULL;
-        node->ptr2 = NULL;
-        
-        return node;
-    }
+{
+    struct tree_node *node = (struct tree_node*)malloc(sizeof(struct tree_node));
+    if(!node)  
+        printf("malloc failed");
+    node->type = 1;
+    node->value = 0;
+    node->name = name;
+    node->arglist = NULL;
+    node->ptr1 = NULL;
+    node->ptr2 = NULL;
+    
+    return node;
+}
 
+//Create leaf nodes for NUMBERS
 struct tree_node* mkNUM(int val)
-    {
-        struct tree_node* node = (struct tree_node*)malloc(sizeof(struct tree_node));
-        if(!node)
-            printf("malloc failed");
-        node->type = 0;
-        node->value = val;
-        node->name = 0;
-        node->arglist = NULL;
-        node->ptr1 = NULL;
-        node->ptr2 = NULL;
-        
-        return node;
-    }
+{
+    struct tree_node* node = (struct tree_node*)malloc(sizeof(struct tree_node));
+    if(!node)
+        printf("malloc failed");
+    node->type = 0;
+    node->value = val;
+    node->name = 0;
+    node->arglist = NULL;
+    node->ptr1 = NULL;
+    node->ptr2 = NULL;
+    
+    return node;
+}
+
+//Evaluates expression nodes
 int exp_evaluate(struct tree_node* node)
+{
+    switch(node->type)
     {
-        switch(node->type)
-        {
-            case CNUMBER:
-                return node->value;
-                break;
-            case CID:
-                if(initialized[node->name-'a'])
-                    return variables[node->name-'a'];
-                else
-                {
-                    printf("uninitialized variable used!!");
-                    exit(1);
-                    break;
-                }
-            case '+':
-                return exp_evaluate(node->ptr1) + exp_evaluate(node->ptr2);
-                break;
-            case '-':
-                return exp_evaluate(node->ptr1) - exp_evaluate(node->ptr2);
-                break;
-            case '/':
-                return exp_evaluate(node->ptr1) - exp_evaluate(node->ptr2);
-                break;
-            case '*':
-                return exp_evaluate(node->ptr1) * exp_evaluate(node->ptr2);
-                break;
-            case '<':
-                if(exp_evaluate(node->ptr1) < exp_evaluate(node->ptr2))
-                    return 1;
-                else
-                    return 0;
-                break;
-            case '>':
-                if(exp_evaluate(node->ptr1) > exp_evaluate(node->ptr2))
-                    return 1;
-                else
-                    return 0;
-                break;
-            case ISEQUAL:
-                if(exp_evaluate(node->ptr1) == exp_evaluate(node->ptr2))
-                    return 1;
-                else
-                    return 0;
-                break;
-        }
-    }
-
-void evaluate(struct tree_node* node)
-    {
-        if(node->type == CSLIST)
-        {
-            evaluate(node->ptr1);
-            evaluate(node->ptr2);
-        }
-        else if(node->type == ASSG)
-        {
-            int rhs = exp_evaluate(node->ptr2);
-            variables[node->ptr1->name -'a'] = rhs;
-            initialized[node->ptr1->name-'a'] = 1;
-        }
-        else if(node->type == CREAD)
-        {
-            scanf("%d",&variables[node->arglist->name -'a']);
-            initialized[node->arglist->name-'a'] = 1;
-        }
-        else if(node->type == CWRITE)
-        {
-            int rhs = exp_evaluate(node->arglist);
-            printf("%d\n",rhs);
-        }
-        else if(node->type == CIF)
-        {
-            if(exp_evaluate(node->ptr1))
-                evaluate(node->ptr2);
-        }
-        else if(node->type == CWHILE)
-        {
-            int ret = exp_evaluate(node->ptr1);
-            while(exp_evaluate(node->ptr1))
+        case CNUMBER:
+            return node->value;
+            break;
+        case CID:
+            if(initialized[node->name-'a'])
+                return variables[node->name-'a'];
+            else
             {
-                evaluate(node->ptr2);               
-                ret = exp_evaluate(node->ptr1);
-             }
-        }
-            return;
+                printf("uninitialized variable used!!");
+                exit(1);
+                break;
+            }
+        case '+':
+            return exp_evaluate(node->ptr1) + exp_evaluate(node->ptr2);
+            break;
+        case '-':
+            return exp_evaluate(node->ptr1) - exp_evaluate(node->ptr2);
+            break;
+        case '/':
+            return exp_evaluate(node->ptr1) - exp_evaluate(node->ptr2);
+            break;
+        case '*':
+            return exp_evaluate(node->ptr1) * exp_evaluate(node->ptr2);
+            break;
+        case '<':
+            if(exp_evaluate(node->ptr1) < exp_evaluate(node->ptr2))
+                return 1;
+            else
+                return 0;
+            break;
+        case '>':
+            if(exp_evaluate(node->ptr1) > exp_evaluate(node->ptr2))
+                return 1;
+            else
+                return 0;
+            break;
+        case ISEQUAL:
+            if(exp_evaluate(node->ptr1) == exp_evaluate(node->ptr2))
+                return 1;
+            else
+                return 0;
+            break;
     }
+}
 
+//Evaluates abstract syntax tree
+void evaluate(struct tree_node* node)
+{
+    if(node->type == CSLIST)
+    {
+        evaluate(node->ptr1);
+        evaluate(node->ptr2);
+    }
+    else if(node->type == ASSG)
+    {
+        int rhs = exp_evaluate(node->ptr2);
+        variables[node->ptr1->name -'a'] = rhs;
+        initialized[node->ptr1->name-'a'] = 1;
+    }
+    else if(node->type == CREAD)
+    {
+        scanf("%d",&variables[node->arglist->name -'a']);
+        initialized[node->arglist->name-'a'] = 1;
+    }
+    else if(node->type == CWRITE)
+    {
+        int rhs = exp_evaluate(node->arglist);
+        printf("%d\n",rhs);
+    }
+    else if(node->type == CIF)
+    {
+        if(exp_evaluate(node->ptr1))
+            evaluate(node->ptr2);
+    }
+    else if(node->type == CWHILE)
+    {
+        int ret = exp_evaluate(node->ptr1);
+        while(exp_evaluate(node->ptr1))
+        {
+            evaluate(node->ptr2);               
+            ret = exp_evaluate(node->ptr1);
+            }
+    }
+        return;
+}
--- stage3-onwards/exptree.h
+++ stage3-onwards/exptree.h
@@ -1,26 +1,26 @@
 //Constants for the type attribute of the tree node
-#define CNUMBER 0
-#define CID 1
-#define CSLIST 2
-#define ASSG 3
-#define CREAD 4
-#define CWRITE 5
-#define CWHILE 6
-#define CIF 7
-#define ISEQUAL 8
+#define CNUMBER 0   //NUMBER
+#define CID 1       //IDENTIFIER
+#define CSLIST 2    //STATEMENT LIST
+#define ASSG 3      //ASSIGNMENT ID = E
+#define CREAD 4     //READ(ID)
+#define CWRITE 5    //WRITE(E)
+#define CWHILE 6    //WHILE(COND)
+#define CIF 7       //IF(COND)
+#define ISEQUAL 8   //==
 
-struct tree_node{
-        //type : 0 - NUMBER, 1 - ID ,2 - SLIST, 3 - ASSG, 4 - READ, 5 - WRITE, '+' - Addition, '*'
-        //-Multiplication, '-' - Subtraction
-        int type;
-        //if type is 1, name defines identifier name
-        char name;
-        //integer value of number at node, valid only if type is 0
-        int value;
-        //argument name
-        struct tree_node *arglist;
-        //ptr1 and ptr2 are NULL for leaf nodes
-        struct tree_node *ptr1, *ptr2;
+struct tree_node{ 
+    //type takes one of the constants defined above and denotes the type of node
+    int type;
+    //name is required if node is of type CID
+    char name;
+    //integer value of number at node, valid only if type is CNUMBER
+    int value;
+    //list of nodes passed as arguments to functions 
+    struct tree_node *arglist;
+    //ptr1 and ptr2 are pointers to child nodes
+    //NULL in case of leaf nodes (NUM or ID)
+    struct tree_node *ptr1, *ptr2;
     };
 
 struct tree_node* mkOpNode(int op, struct tree_node* ptr1, struct tree_node* ptr2);
@@ -35,6 +35,8 @@ int exp_evaluate(struct tree_node* node);
 
 void evaluate(struct tree_node* node);
 
+//table for storing the current value for the variables a-z
 int variables[26];
 
+//table for checking whether variables are initialized
 int initialized[26];
